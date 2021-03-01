35bf27bc78c81b9e8e6aa991174cad87b9df0d2f
EpicCodingMaster <EyresMac@Jeffreys-iMac.local>
2021-02-28 18:26:06 -0600

    change

--- hangman.py
+++ hangman.py
@@ -1,6 +1,6 @@
-# The goal of this project is to replicate hangman using python
+# The  goal of this project is to replicate hangman using python
 # A sample file is provided that should help with printing the hangman in the terminal
-
+    
 def terminal(misses):
     initial = ["   ____________", "   | /         ", "   |/          ", "   |           ", "   |           ", "   |           ", "   |           ", "   |           ", "   |           ", "   |           ", "   |           ", "___|__________"]
     final = ["   ____________", "   | /        |", "   |/         |", "   |         ( )", "   |         _|_", "   |        |   |", "   |     <--|   |-->", "   |        |___|","   |         / \ ","   |        /   \\","   |      o|     |o", "___|__________"]
@@ -9,8 +9,8 @@ def terminal(misses):
             print(final[val])
         else:
             print(initial[val])
-
-
+  
+ 
 if __name__ == "__main__":
     for val in range(12):
         terminal(val)
